6d2c49ecda643d8814c7b90b5a9a9c75e9c23c26
Anbarasu <Anbarasu@themajesticpeople.com>
2026-03-27 12:47:46 +0530

    UPDATED

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import "./App.css";
 import GiftEnvelope from "./GiftEnvelope";
+import AccessibilityWidget from "./AccessibilityWidget";
+import AccessibilityTestPage from "./AccessibilityTestPage";
 
 function App() {
   return (
     <div className="App">
-      <GiftEnvelope imageUrl="https://picsum.photos/300/200" />
+      <AccessibilityWidget />
+      <AccessibilityTestPage />
+      {/* <GiftEnvelope imageUrl="https://picsum.photos/300/200" /> */}
     </div>
   );
 }
