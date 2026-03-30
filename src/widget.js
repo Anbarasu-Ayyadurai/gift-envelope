78524059d9a548a2059b4b675647e73c99067137
Anbarasu <Anbarasu@themajesticpeople.com>
2026-03-30 14:47:37 +0530

    Style FIle Added

--- src/widget.js
+++ src/widget.js
@@ -1,11 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css";
-import "./App.css";
+import styles from "./styles";
 
 (function () {
+
+    // ✅ Inject CSS manually
+    const injectStyles = () => {
+        if (document.getElementById("my-widget-styles")) return;
+
+        const styleTag = document.createElement("style");
+        styleTag.id = "my-widget-styles";
+        styleTag.innerHTML = styles;
+
+        document.head.appendChild(styleTag);
+    };
+
     const mount = (containerId, props = {}) => {
+        injectStyles(); // 🔥 important
+
         const el = document.getElementById(containerId);
 
         if (!el) {
@@ -17,8 +30,8 @@ import "./App.css";
         root.render(<App {...props} />);
     };
 
-    // 🔥 FORCE GLOBAL (this WILL NOT be overridden)
     window.MyReactWidget = {
         mount
     };
+
 })();
