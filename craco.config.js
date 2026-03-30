7c88ce234a74be29ba84842d9f0814c9ac4e0910
Anbarasu <Anbarasu@themajesticpeople.com>
2026-03-30 14:33:26 +0530

    Updated

--- craco.config.js
+++ craco.config.js
@@ -1,10 +1,7 @@
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-
 module.exports = {
     webpack: {
         configure: (config) => {
 
-            // ✅ Entry for widget
             config.entry = './src/widget.js';
 
             config.output = {
@@ -12,36 +9,13 @@ module.exports = {
                 filename: 'widget.js'
             };
 
-            // ✅ Disable splitting
             config.optimization.splitChunks = {
                 cacheGroups: {
                     default: false
                 }
             };
+
             config.optimization.runtimeChunk = false;
-
-            // ✅ Remove MiniCssExtractPlugin safely
-            config.plugins = config.plugins.filter(
-                plugin => !(plugin instanceof MiniCssExtractPlugin)
-            );
-
-            // ✅ Replace CSS loader
-            config.module.rules.forEach(rule => {
-                if (Array.isArray(rule.oneOf)) {
-                    rule.oneOf.forEach(one => {
-                        if (one.use) {
-                            one.use.forEach(loader => {
-                                if (
-                                    loader.loader &&
-                                    loader.loader.includes("mini-css-extract-plugin")
-                                ) {
-                                    loader.loader = require.resolve("style-loader");
-                                }
-                            });
-                        }
-                    });
-                }
-            });
 
             return config;
         }
