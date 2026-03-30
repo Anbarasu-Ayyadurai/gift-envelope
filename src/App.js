b43bf96ab7ba86bfde43d5196c0d16bf73280800
Anbarasu <Anbarasu@themajesticpeople.com>
2026-03-30 11:43:50 +0530

    Updated code

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
       <AccessibilityWidget />
-      <AccessibilityTestPage />
+      {/* <AccessibilityTestPage /> */}
       {/* <GiftEnvelope imageUrl="https://picsum.photos/300/200" /> */}
     </div>
   );
